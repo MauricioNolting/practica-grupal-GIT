c8182b0f050877b24259219a480d7520d73be4f7
edisonmaya <ee_mayahuayamave2@hotmail.com>
2023-10-13 22:40:23 -0500

    Change project EdisonMaya

--- src/components/EdisonMaya.jsx
+++ src/components/EdisonMaya.jsx
@@ -1,6 +1,12 @@
 const EdisonMaya = () => {
   return (
-    <div>EdisonMaya</div>
+    <article className=" w-[260px] flex flex-col my-4 p-4 max-w-fit gap-4 border-2 border-white mx-auto rounded-2xl shadow-md shadow-white/50">
+      <header>
+        <img className="w-[200px] h-[200px] object-cover mx-auto rounded-full shadow-sm shadow-white border-1" src="/EdisonMaya.jpeg" alt="" />
+      </header>
+      <h3 className="font-bold">Edison Maya</h3>
+      <p>Tecnologo Desarrollo Sotfware actualmente estudiante de Academlo</p>
+    </article>
   )
 }
 export default EdisonMaya
