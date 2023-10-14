bc379afb051c5adda2d370f84204cb6fea37aea9
Mauricio Nolting <mauricionolting@gmail.com>
2023-10-14 00:34:55 -0300

    style/mauricionolting

--- src/components/MauricioNolting.jsx
+++ src/components/MauricioNolting.jsx
@@ -1,6 +1,12 @@
 const MauricioNolting = () => {
   return (
-    <div>MauricioNolting</div>
+    <article className="py-20 max-w-[400px] mx-auto ">
+        <header className="w-[300px] mx-auto rounded-xl overflow-hidden border-4">
+            <img className="" src="/mauricionolting/mn.jpeg" alt="" />
+        </header>
+        <h3 className="italic text-xl font-bold">Mauricio Nolting</h3>
+        <p className="max-w-[400px] mx-auto">Estudiante ACADEMBLO</p>
+    </article>
   )
 }
 export default MauricioNolting
