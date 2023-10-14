84a3485a1142aaf6e18543be3f21e84dfcdf361f
JonatanCruzz <jonatanalexanderdiaz@gmail.com>
2023-10-13 23:32:22 -0400

    fix(JonatanCruz.jsx): the color of the letters, the size of the title and the paragraph were changed

--- src/components/JonatanCruz.jsx
+++ src/components/JonatanCruz.jsx
@@ -5,14 +5,14 @@ const PersonalCard = () => {
   const lastName = 'Cruz Diaz';
 
   return (
-    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden p-4">
+    <div className="max-w-md mx-auto bg-green-950 rounded-xl shadow-md overflow-hidden p-4">
       <header>
         <img className="w-48 rounded-full mx-auto" src="/jonatan-image.png" alt="" />
       </header>
-      <h2 className="text-2xl font-semibold">
+      <h2 className="text-white/90 text-2xl font-semibold">
         {firstName} {lastName}
       </h2>
-      <p className="text-gray-600 mt-2">
+      <p className="text-white/90 text-xl text-justify mt-4">
         Desarrollador FullStack con 1 año de experiencia con Nest.js,
         JavaScript, React.js bajo Node.js. En el área de Nest.js he trabajado con REST APIS, TypeScript, Mongoose,
         Authentication, Authorization, Docker, MongoDB, Postgres, TypeOrm, Git, GitHub, GitLab. En el área de React.js he trabajado con
